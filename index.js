735c35ea7a335734693fa5021f3ab3a32b87c3d9
klltx <kostya.svetashov.01@gmail.com>
2023-08-31 17:30:05 +0400

    fix task manager

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const adminId = +process.env.ADMIN_ID;
 const channelLink = process.env.CHANNEL_LINK;
 const bot = new TelegramBot(token, { polling: true });
 
-cron.schedule("0, 30 * * * *", () => {
+cron.schedule("0,30 * * * *", () => {
   const date = new Date();
   const time = `${date.getDate()}  ${date.getHours()}:${date.getMinutes()}`;
   console.log(`${time} - Fetching post...`);
